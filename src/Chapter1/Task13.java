135ec0f8099ad6c4933b738cb189221efdc594c5
NikkaNamoradze <107554272+NikkaNamoradze@users.noreply.github.com>
2024-03-11 17:51:25 +0400

    hmm

--- src/Chapter1/Task13.java
+++ src/Chapter1/Task13.java
@@ -1,14 +1,14 @@
-package Chapter1;
-
-public class Task13 {
-    public static void main(String[] args) {
-        double a = 3.4, b = 50.2, e = 44.5;
-        double c = 2.1, d = 0.55, f = 5.9;
-
-        double x = (e*d-b*f)/(a*d-b*c);
-        double y = (a*f-e*c)/(a*d-b*c);
-
-        System.out.println(x);
-        System.out.println(y);
-    }
-}
+//package Chapter1;
+//
+//public class Task13 {
+//    public static void main(String[] args) {
+//        double a = 3.4, b = 50.2, e = 44.5;
+//        double c = 2.1, d = 0.55, f = 5.9;
+//
+//        double x = (e*d-b*f)/(a*d-b*c);
+//        double y = (a*f-e*c)/(a*d-b*c);
+//
+//        System.out.println(x);
+//        System.out.println(y);
+//    }
+//}????
